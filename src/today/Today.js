43772151a478f3fbcc05d062c9348ffcc1ad2f57
Jordan Bishop <bishopj1292@gmail.com>
2022-07-25 11:38:56 -0500

    Some RWA styles and media queries

--- src/today/Today.js
+++ src/today/Today.js
@@ -2,12 +2,13 @@ import './Today.css';
 
 const Today = ({ forecast, isFahrenheit }) => {
     const fahrenheitTemp = Math.round(1.8 * (forecast?.temp -273.15) + 32);
-    const celciusTemp = Math.round(forecast?.temp -273.15);    
+    const celciusTemp = Math.round(forecast?.temp -273.15); 
+    const mainIcon = forecast?.weather[0].description === 'few clouds' ? 'few_clouds' : forecast?.weather[0].main;   
 
     return (
         <div className="today">
             <div className="temperature">{isFahrenheit ? fahrenheitTemp : celciusTemp}°</div>
-            <div className="main-weather-icon" style={{ backgroundImage: `url(/${forecast?.weather[0].main}.png)`, backgroundSize: "40px", height: 40, width: 40}}></div>
+            <div className="main-weather-icon" style={{ backgroundImage: `url(/${mainIcon}.png)`, backgroundSize: "40px", height: 40, width: 40}}></div>
             <div className="current-details">
                 <div>{forecast?.weather[0].description}</div>
                 <div>{Math.round(forecast?.wind_speed)} mph</div>
